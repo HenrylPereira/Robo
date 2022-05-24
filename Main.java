2b0a2a2b539b03200385c1c362b9b11b99f1a4c5
Henry Lopes Pereira <73833452+HenrylPereira@users.noreply.github.com>
2022-05-24 19:42:16 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    Robo robo = new Robo(100);
+    System.out.println(robo.andarFrente());
+    System.out.println(robo.getBateria());
+  }
+}
